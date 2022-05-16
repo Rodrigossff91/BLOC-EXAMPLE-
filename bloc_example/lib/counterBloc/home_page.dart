c7f0c8a2718cde4feeca59ef28d8a11d1aaca0ab
Rodrigo Ferreira <rodrigo@treeinova.com>
2022-05-16 15:32:24 -0300

    Bloc

---
+++ bloc_example/lib/counterBloc/home_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(''),
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed('/bloc');
+                },
+                child: const Text('Bloc')),
+            TextButton(onPressed: () {}, child: const Text('Cubit'))
+          ],
+        )));
+  }
+}
